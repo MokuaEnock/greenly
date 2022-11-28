f39e67cbd2b86eba3b534f9dcba47b3ef51a7aed
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-28 23:32:49 +0300

    create skeleton for the header

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -2,13 +2,18 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <header>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About Us</NavLink>
-      <NavLink to="/services">Services</NavLink>
-      <NavLink to="/process">Process</NavLink>
-      <NavLink to="/events">Events</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/auth">Log In</NavLink>
+      <div id="header-logo"></div>
+      <div id="header-info">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About Us</NavLink>
+        <NavLink to="/services">Services</NavLink>
+        <NavLink to="/process">Process</NavLink>
+        <NavLink to="/events">Events</NavLink>
+      </div>
+      <div id="header-forms">
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/auth">Log In</NavLink>
+      </div>
     </header>
   );
 }
