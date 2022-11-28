e267f73a0af48c41c6133a753e5b2a9941032279
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-29 00:47:15 +0300

    create skeleton for tagline and images on landing page

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,5 +1,24 @@
 import React from "react";
+import "./landing.css";
 
 export default function Landing() {
-  return <main>Landing pages</main>;
+  return (
+    <main id="landing">
+      <section id="tagline">
+        <span id="title">Towards a greener Nation</span>
+        <span id="paragraph">
+          There should be millions of people striving towards zero waste <br />
+          rather than a select few who achieve it perfectly.
+        </span>
+      </section>
+
+      <section id="images">
+        <div className="recycle-image" id="recycle-image1"></div>
+        <div id="recycle-icon"></div>
+        <div className="recycle-image" id="recycle-image2"></div>
+      </section>
+      <section id="impact"></section>
+      <section id="assistance"></section>
+    </main>
+  );
 }
