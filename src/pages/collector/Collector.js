7a2e250a9ffdeac463da115e7ac39472f3afdf60
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-01 09:08:07 +0300

    create colletcor components and create them

---
+++ src/pages/collector/Collector.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CollectorHome from "./CollectorHome";
+import Collectorprofile from "./CollectorProfile";
+
+export default function Collector() {
+  return (
+    <>
+      <CollectorHome />
+      <Collectorprofile />
+    </>
+  );
+}
