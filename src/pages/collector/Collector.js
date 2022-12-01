8b90458c7a64e7eed3f342380b5a68fe4c9befe8
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-01 09:55:50 +0300

    create general styling for collector page

--- src/pages/collector/Collector.js
+++ src/pages/collector/Collector.js
@@ -1,12 +1,24 @@
+import "./collector.css";
 import React from "react";
 import CollectorHome from "./CollectorHome";
 import Collectorprofile from "./CollectorProfile";
+import { Link } from "react-router-dom";
 
 export default function Collector() {
+  function CollectorBar() {
+    return (
+      <div id="collector-bar">
+        <Link to="#home">Home</Link>
+        <Link to="#profile">Profile</Link>
+      </div>
+    );
+  }
+
   return (
-    <>
+    <main id="collector">
+      <CollectorBar />
       <CollectorHome />
       <Collectorprofile />
-    </>
+    </main>
   );
 }
--- src/pages/collector/CollectorHome.js
+++ src/pages/collector/CollectorHome.js
@@ -1,3 +1,3 @@
 export default function CollectorHome() {
-  return <></>;
+  return <section></section>;
 }
