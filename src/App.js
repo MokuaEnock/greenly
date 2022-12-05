a49519b8f34b54d216b500afd314e445201143a1
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 02:36:12 +0300

    set user usestate

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import NavBar from "./pages/navbar";
 import Landing from "./pages/landing/landing";
 import Footer from "./pages/footer";
 import About from "./pages/about/about";
-import Services from "./pages/services/services";
-import Events from "./pages/events/events";
+// import Services from "./pages/services/services";
+// import Events from "./pages/events/events";
 import Process from "./pages/process/process";
-import Contact from "./pages/contact/contact";
+// import Contact from "./pages/contact/contact";
 import Auth from "./pages/auth/auth";
 import Login from "./pages/auth/login";
 import Collector from "./pages/collector/Collector";
 import Disposer from "./pages/disposer/Disposer";
+import { useState } from "react";
 
 function App() {
+  let [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
       <NavBar />
