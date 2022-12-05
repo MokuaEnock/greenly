18468abb3e1fe01f55c779912e4c52848c744460
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 01:27:06 +0300

    create login skeleton

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -2,7 +2,13 @@ import "./auth.css";
 export default function Login() {
   return (
     <main className="auth">
-      <form id="login">Login</form>
+      <form id="login">
+        <span>Login</span>
+        <input type="text" placeholder="Enter username" />
+        <input type="email" placeholder="Enter email" />
+        <input type="password" placeholder="Enter password" />
+        <button type="submit">Continue</button>
+      </form>
     </main>
   );
 }
