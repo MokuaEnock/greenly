b58d5aa204da4d9811fd8da23c8f4a24d27f830b
Enock Mokua <enock.mokua@student.moringaschool.com>
2022-12-14 17:56:11 +0300

    add info sections and background image

--- src/pages/buyers/BuyerHome.js
+++ src/pages/buyers/BuyerHome.js
@@ -1,3 +1,4 @@
+import Agric from "../images/agriculture.jpg";
 export default function BuyerHome() {
   return (
     <main id="buyer-home">
@@ -12,9 +13,18 @@ export default function BuyerHome() {
 
       <section id="compost-container">
         <div className="compost-card">
-          <span className="compost-card-image"></span>
-          <span className="compost-card-info"></span>
-          <span className="compost-card-desc"></span>
+          <span className="compost-card-image">
+            <img src={Agric} alt="compost" />
+          </span>
+          <span className="compost-card-info">
+            <button className="infos">Potassium</button>
+            <button className="infos">Moisture</button>
+            <button className="infos">Discounted</button>
+          </span>
+          <span className="compost-card-desc">
+            Compost, crumbly mass of rotted organic matter made from decomposed
+            plant material, used in gardening and agriculture.
+          </span>
           <span className="compost-card-buttons">
             <button className="price">Ksh 150/kg</button>
             <button className="order">Order</button>
