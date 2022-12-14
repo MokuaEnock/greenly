3c0791cf70cb45b64f30cf4c018acbff0e1dfa1d
Enock Mokua <enock.mokua@student.moringaschool.com>
2022-12-14 10:56:31 +0300

    redirect components

--- src/pages/redirect/redirect.js
+++ src/pages/redirect/redirect.js
@@ -5,6 +5,9 @@ export default function Redirect() {
   return (
     <main id="redirect">
       <Link className="redirects"></Link>
+      <Link className="redirects"></Link>
+      <Link className="redirects"></Link>
+      <Link className="redirects"></Link>
     </main>
   );
 }
