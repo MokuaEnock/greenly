e67753bcab4d9395f1d01e1171e9b7eea13181e1
Enock Mokua <enock.mokua@student.moringaschool.com>
2022-12-14 12:05:06 +0300

    create skeleton for buyer

--- src/pages/buyers/BuyerHome.js
+++ src/pages/buyers/BuyerHome.js
@@ -1,5 +1,12 @@
 export default function BuyerHome() {
-  return <main id="buyer-home">
-    
-  </main>;
+  return (
+    <main id="buyer-home">
+      <section id="buyer-title">
+        <span id="buyer-heading">Hello there Enok want to buy?</span>
+        <span id="buyer-par">
+          Purchase quality and quantity compost from us.
+        </span>
+      </section>
+    </main>
+  );
 }
