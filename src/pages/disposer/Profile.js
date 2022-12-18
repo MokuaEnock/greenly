195e945ae48b795bf1771512e828140732d75ad8
Enock <enock.mokua@student.moringaschool.com>
2022-12-18 14:30:30 +0300

    create gender and dat of birth inputs

--- src/pages/disposer/Profile.js
+++ src/pages/disposer/Profile.js
@@ -23,7 +23,69 @@ export default function Profile() {
             <input type="text" placeholder="Last name" />
           </span>
         </div>
-        
+
+        <div className="form-cont">
+          <span className="inputs">
+            <label>Select your gender.</label>
+            <select id="age">
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </span>
+
+          <span className="inputs">
+            <label>Enter your date of birth.</label>
+            <input type="date" id="birthday" name="birthday" />
+          </span>
+        </div>
+
+        <div className="form-cont">
+          <span className="inputs">
+            <label>Enter county</label>
+            <select></select>
+          </span>
+          <span className="inputs">
+            <label>Second name</label>
+            <input type="text" placeholder="Second name" />
+          </span>
+          <span className="inputs">
+            <label>Last name</label>
+            <input type="text" placeholder="Last name" />
+          </span>
+        </div>
+
+        <div className="form-cont">
+          <span className="inputs">
+            <label>Select your gender.</label>
+            <select id="age">
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </span>
+          <span className="inputs">
+            <label>Second name</label>
+            <input type="text" placeholder="Second name" />
+          </span>
+          <span className="inputs">
+            <label>Last name</label>
+            <input type="text" placeholder="Last name" />
+          </span>
+        </div>
+
+        <div className="form-cont">
+          <span className="inputs">
+            <label>First name</label>
+            <input type="text" placeholder="FIrst name" />
+          </span>
+          <span className="inputs">
+            <label>Second name</label>
+            <input type="text" placeholder="Second name" />
+          </span>
+          <span className="inputs">
+            <label>Last name</label>
+            <input type="text" placeholder="Last name" />
+          </span>
+        </div>
       </form>
     </main>
   );
