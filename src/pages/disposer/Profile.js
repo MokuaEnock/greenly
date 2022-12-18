da078f7018f773c93f0dacd6067a83fdaeae5394
Enock <enock.mokua@student.moringaschool.com>
2022-12-18 09:22:04 +0300

    add profile title

--- src/pages/disposer/Profile.js
+++ src/pages/disposer/Profile.js
@@ -1,5 +1,14 @@
-import "./disposer.css";
+import "./profile.css";
 
 export default function Profile() {
-  return <main id="poser-profile">Disposer Profile</main>;
+  return (
+    <main id="poser-profile">
+      <section id="profile-title">
+        <span id="profile-head">We would like to get some data from you.</span>
+        <span id="profile-par">Read our privacy policy</span>
+      </section>
+
+      <form id="profile-form"></form>
+    </main>
+  );
 }
